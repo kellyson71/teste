a4bac53975873986a0903b2196cb988120817000
kellyson <kellyson.medeiros.pdf@gmail.com>
2024-02-09 12:25:03 -0300

    teste

---
+++ lenny/livro/script.js
@@ -0,0 +1,28 @@
+let currentPage = 1;
+
+function toggleClass(e, toggleClassName) {
+  if(e.className.includes(toggleClassName)) {
+    e.className = e.className.replace(' ' + toggleClassName, '');
+  } else {
+    e.className += ' ' + toggleClassName;
+  }
+}
+
+function movePage(e, page) {
+  if (page == currentPage) {
+    currentPage+=2;
+    toggleClass(e, "left-side");
+    toggleClass(e.nextElementSibling, "left-side");
+    
+  }
+  else if (page = currentPage - 1) {
+    currentPage-=2;
+    toggleClass(e, "left-side");
+    toggleClass(e.previousElementSibling, "left-side");
+  }
+  
+}
+document.getElementById("redirectButton").addEventListener("click", function() {
+  // Substitua 'pagina-de-destino.html' pela URL da página para a qual você deseja redirecionar
+  window.location.href = "/index.html";
+});
