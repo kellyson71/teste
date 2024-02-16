2d5877d7b803ec109a17e77081f3e6b80b0d2694
kellyson <kellyson.medeiros.pdf@gmail.com>
2024-02-16 12:23:16 -0300

    a

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+function validateLogin() {
+  var emailInput = document.getElementById("email").value;
+  var passwordInput = document.getElementById("password").value;
+
+  // Check if the entered email and password match the specified values
+  if (emailInput === "kellyson.medeiros.pdf@gmail.com" && passwordInput === "kellyson123") {
+      // Redirect to another page
+      window.location.href = "another_page.html";
+  } else {
+      alert("ainda vou fazer a pagina de cadastro");
+  }
+}
+
+function forgotPassword() {
+  alert("como vc esqueceu a senha? nem conta vc tem"); // Display a message when "Esqueci minha senha" is clicked
+}
+
 const capas = [
   './capas/cap1.png',
   './capas/cap2.png',
